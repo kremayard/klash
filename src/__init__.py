fc0ae9ffc4e6840b39405932a78d42dede42db4b
5elenay <5elenay@protonmail.com>
2021-08-06 20:34:41 +0300

    ✨  added add slash command decorator.

--- src/__init__.py
+++ src/__init__.py
@@ -2,8 +2,39 @@
 Klash source file.
 """
 
+import asyncio
+from typing import Optional, Callable
+
+
 class Klash:
     """Base class for Klash."""
 
     def __init__(self) -> None:
-        self.__commands: list = []
+        self.commands: list = []
+        self._ids: list = []
+
+    def add(self, guild_id: Optional[int], name: Optional[str], description: str, options: list):
+        """Add new global slash command decorator.
+
+        Args:
+            guild_id (int, None): Guild ID. (if None, it will be global command.).
+            name (str, None): Command name. (if None, it will get the function name as command name.).
+            description (str): Command description
+            options (list): Commad options. (see https://discord.com/developers/docs/interactions/slash-commands#application-command-object-application-command-option-structure.)
+        """
+
+        def decorator(fn: Callable):
+            def wrapper():
+                self.commands.append({
+                    "scope": "global" if guild_id is None else guild_id,
+                    "name": name or fn.__name__,
+                    "description": description,
+                    "options": options,
+                    "execute": fn
+                })
+
+                return self.commands
+
+            return wrapper()
+
+        return decorator
